433e56ef8084b17c14792856624bc045cc5463c7
Andres Felipe Salamanca Ramos <138737046+Andrxo16@users.noreply.github.com>
2024-10-28 18:37:11 -0500

    archivos 2.0

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Home from "./assets/pages/home/Home.jsx";
 import RecyCling from "./assets/pages/recycling/Recycling.jsx";
 import Page404 from "./assets/pages/page-404/Page404.jsx";
 import Erosion from "./assets/pages/earth/soil-erosion/erosion.jsx";
+import Management from "./assets/pages/earth/improper-waste-management/management.jsx";
 import Map from "./assets/pages/map/map.jsx";
 import Background from "./assets/pages/earth/soil-erosion/prueba/Background.jsx";
 
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
   { path: "/Quiz", element: <Quiz />,},
   { path: "/home", element: <Home /> },
   {path: "/soil-erosion", element: <Erosion/>},
+  {path: "/management", element: <Management/>},
   { path: "/recycling", element: <RecyCling /> },
   { path: "*", element: <Page404 /> },
   { path: "/map", element: <Map /> },
