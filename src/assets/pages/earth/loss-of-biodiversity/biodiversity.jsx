67d37270b66a73d7bfe2d6a92ae65d3ae5e71105
Jonatan Medina <Jonatan.Medina@correounivalle.edu.co>
2024-10-27 22:09:48 -0500

    añadiendo nuevo punto para bosquejo

---
+++ src/assets/pages/earth/loss-of-biodiversity/biodiversity.jsx
@@ -0,0 +1,13 @@
+  import Header from "../../../../components/Header/Header";
+
+  const biodiversity = () => {
+    return (
+      <>
+        <Header />
+        <h1>hola Biodiversidad</h1>
+      </>
+    );
+  };
+  
+  export default biodiversity;
+  
