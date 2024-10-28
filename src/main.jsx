7499ae3cebaa3270dca08402b5a0950861cb42e0
Jonatan Medina <Jonatan.Medina@correounivalle.edu.co>
2024-10-27 22:05:50 -0500

    anadiendo nuevas rutas para los demas entorno

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import Home from "./assets/pages/home/Home.jsx";
 import RecyCling from "./assets/pages/recycling/Recycling.jsx";
 import Page404 from "./assets/pages/page-404/Page404.jsx";
 import Erosion from "./assets/pages/earth/soil-erosion/erosion.jsx";
+import Deforestation  from "./assets/pages/earth/Deforestation/deforestation.jsx";
+import Biodiversity  from "./assets/pages/earth/loss-of-biodiversity/biodiversity.jsx";
 import Map from "./assets/pages/map/map.jsx";
 
 const router = createBrowserRouter([
@@ -15,7 +17,9 @@ const router = createBrowserRouter([
   { path: "/login",element: <Login />,},
   { path: "/Quiz", element: <Quiz />,},
   { path: "/home", element: <Home /> },
-  {path: "/soil-erosion", element: <Erosion/>},
+  { path: "/soil-erosion", element: <Erosion/>},
+  { path: "/deforestation", element: <Deforestation /> },
+  { path: "/biodiversity", element: <Biodiversity /> },
   { path: "/recycling", element: <RecyCling /> },
   { path: "*", element: <Page404 /> },
   { path: "/map", element: <Map /> },
