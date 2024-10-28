5b225a6bd723ea67305f251601f84f84efa4510d
Jonatan Medina <Jonatan.Medina@correounivalle.edu.co>
2024-10-27 22:08:37 -0500

    usando bosquejos para un proximo avance mas completo

---
+++ src/assets/pages/earth/Deforestation/deforestation.jsx
@@ -0,0 +1,14 @@
+import Header from "../../../../components/Header/Header";
+
+const Deforestation = () => {
+  return (
+    <>
+      <Header />
+      <h1>hola deforestation</h1>
+    </>
+  );
+};
+
+export default Deforestation;
+
+  
