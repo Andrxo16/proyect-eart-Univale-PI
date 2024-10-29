9115af403ab105435540bf0a47fd21b3791397a5
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-10-28 20:57:18 -0500

    jcgs
    flash 3.0.2

--- src/assets/pages/earth/improper-waste-management/management.jsx
+++ src/assets/pages/earth/improper-waste-management/management.jsx
@@ -1,8 +1,7 @@
 import Header from "../../../../components/Header/Header";
+import RecyCling from "../../recycling/Recycling";
 import "./management.css";
 
-import { Canvas } from "@react-three/fiber"; // Importa el componente Canvas que sirve como contenedor para renderizar el contenido 3D
-import { OrbitControls, useGLTF } from "@react-three/drei"; // Importa OrbitControls para permitir el control del modelo con el ratón y useGLTF para cargar modelos GLTF
 
 const Management = () => {
     return (
@@ -17,8 +16,6 @@ const Management = () => {
             <div className="home-erosion">
                 <h1 className="tituloPrincipal">El manejo de residuos se refiere a la recolección, transporte, procesamiento y disposición de los desechos generados por actividades humanas. Cuando no se maneja adecuadamente, puede generar graves problemas ambientales, como contaminación del suelo, agua y aire, contribuyendo al cambio climático y afectando la salud humana.</h1>
             </div>
-
-
 
 
         </>
